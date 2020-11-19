4e5f69d52be683850228a154c8741fac16916ec4
Mick Styers <68083989+mqqk@users.noreply.github.com>
2020-11-18 19:02:39 -0500

    idk what i'm doing

--- src/app.js
+++ src/app.js
@@ -22,9 +22,9 @@ app.use(cors())
 app.use('/api/folders', foldersRouter)
 app.use('/api/notes', notesRouter)
 
-app.get('/', (req,res) =>{
-    res.send('Hello, world!')
-})
+// app.get('/', (req,res) =>{
+//     res.send('Hello, world!')
+// })
 
 
 
